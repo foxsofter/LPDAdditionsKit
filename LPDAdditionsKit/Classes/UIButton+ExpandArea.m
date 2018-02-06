281e31b127c5b40bae199192d0d012798a98bbe7
yonguang.li <yongguang.li@ele.me>
2018-02-06 16:56:33 +0800

    [CHG]代码优化

--- LPDAdditionsKit/Classes/UIButton+ExpandArea.m
+++ LPDAdditionsKit/Classes/UIButton+ExpandArea.m
@@ -8,46 +8,32 @@
 #import "UIButton+ExpandArea.h"
 #import <objc/runtime.h>
 
-static const NSString *top = @"top";
-static const NSString *left = @"left";
-static const NSString *bottom = @"bottom";
-static const NSString *right = @"right";
-
 @implementation UIButton (ExpandArea)
 
 - (void)setExpandedAreaEdge:(UIEdgeInsets)edge {
-  [self _setExpandedValue:edge.top forKey:&top];
-  [self _setExpandedValue:edge.left forKey:&left];
-  [self _setExpandedValue:edge.bottom forKey:&bottom];
-  [self _setExpandedValue:edge.right forKey:&right];
+  NSValue *valueObj  = [NSValue valueWithUIEdgeInsets:edge];
+  objc_setAssociatedObject(self, _cmd, valueObj, OBJC_ASSOCIATION_RETAIN);
 }
 
+#pragma - override
 
-- (UIEdgeInsets)_expandedAreaEdge {
-  return UIEdgeInsetsMake([self _ExpandedValueForKey:&top], [self _ExpandedValueForKey:&left], [self _ExpandedValueForKey:&bottom], [self _ExpandedValueForKey:&right]);
+- (BOOL)pointInside:(CGPoint)point withEvent:(UIEvent *)event {
+  CGRect expandedClickedRect = [self _expandedClickedRect];
+  return CGRectContainsPoint(expandedClickedRect, point);
 }
+
+#pragma - private
 
 - (CGRect)_expandedClickedRect {
   UIEdgeInsets edge = [self _expandedAreaEdge];
   CGRect expandedClickRect = CGRectMake(self.bounds.origin.x - edge.left, self.bounds.origin.y - edge.top, self.bounds.size.width + edge.right + edge.left, self.bounds.size.height + edge.top + edge.bottom);
-  
   return expandedClickRect;
 }
 
-- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
-  if(!self.userInteractionEnabled || self.hidden || !self.enabled) {
-    return nil;
-  }
-  CGRect expandedClickedRect = [self _expandedClickedRect];
-  return CGRectContainsPoint(expandedClickedRect, point) ? self : nil;
-}
-
-- (void)_setExpandedValue:(CGFloat)value forKey:(void *)key {
-  objc_setAssociatedObject(self, key, @(value), OBJC_ASSOCIATION_RETAIN);
-}
-
-- (CGFloat)_ExpandedValueForKey:(void *)key {
-  return [objc_getAssociatedObject(self, key) floatValue];
+- (UIEdgeInsets)_expandedAreaEdge {
+  NSValue *valueObj = objc_getAssociatedObject(self, @selector(setExpandedAreaEdge:));
+  UIEdgeInsets insets = [valueObj UIEdgeInsetsValue];
+  return insets;
 }
 
 @end
